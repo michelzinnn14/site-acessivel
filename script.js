8c70e89a7406947fc3e34d6d542483f7257169b7
michelzinnn14 <163421867+michelzinnn14@users.noreply.github.com>
2025-03-24 14:15:32 +0000

    botaoAcessibilidade

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = ocument.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        botaoDeAcessibilidade.classList.toggle(apresenta-lista);
+    })
+  
+
     const aumentaFonteBotao = document.getElementById('aumenta-fonte');
     const diminuiFonteBotao = document.getElementById('diminui-fonte');
+    
+
     let tamanhoAtualFonte = 1;
-    aumentaFonteBotao.addEventListener('click',function( ){
+    aumentaFonteBotao.addEventListener('click',function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `$(tamanhoAtualFonte)rem`;
     });
 
     
-    aumentaFonteBotao.addEventListener('click',function( ){
+    diminuiFonteBotao.addEventListener('click',function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `$(tamanhoAtualFonte)rem`;
     });
